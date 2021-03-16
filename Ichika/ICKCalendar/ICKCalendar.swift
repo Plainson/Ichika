44c85dc6d857956515758dddfd79d18bb3e3758a
WangYi <rd331@netvox.com.cn>
2021-03-16 17:00:34 +0800

    ICKCalendar.

--- Ichika/ICKCalendar/ICKCalendar.swift
+++ Ichika/ICKCalendar/ICKCalendar.swift
@@ -8,7 +8,41 @@
 import Foundation
 
 struct ICKCalendar {
-    static var current: Calendar = Calendar.current
     
+    static var current: ICKCalendar = ICKCalendar.init()
     
+    private var calendar: Calendar = Calendar.current
+    
+    init() {
+        self.calendar = Calendar.current
+    }
+    
+    init(calendar: Calendar) {
+        self.calendar = calendar
+    }
+    
+    public func week() -> Int {
+        
+    }
+}
+
+extension ICKCalendar {
+    
+    // MARK: - 获取给定时间的月份的第一天。
+    
+    public func getFirstDateOfMonth(date: ICKDate) -> ICKDate {
+        let dateComponents: DateComponents = self.calendar.dateComponents([.year, .month], from: date.toDate())
+        let firstDateOfMonth: ICKDate = ICKDate.init(date: self.calendar.date(from: dateComponents)!)
+        return firstDateOfMonth
+    }
+    
+    // MARK: - 获取给定时间月份的最后一天。
+    
+    public func getLastDateOfMonth(date: ICKDate) -> ICKDate {
+        var dateComponents: DateComponents = DateComponents.init()
+        dateComponents.month = 1
+        dateComponents.second = -1
+        let lastDateOfMonth: ICKDate = ICKDate.init(date: self.calendar.date(byAdding: dateComponents, to: self.getFirstDateOfMonth(date: date).toDate())!)
+        return lastDateOfMonth
+    }
 }
--- Ichika/ICKCalendar/ICKDate.swift
+++ Ichika/ICKCalendar/ICKDate.swift
@@ -9,7 +9,46 @@ import Foundation
 
 struct ICKDate {
     
+    static var today: ICKDate = ICKDate.init()
+    
+    public var dateFormatterString: String = "yyyy-MM-dd"
+    
+    private var date: Date!
+    private var dateFormatter: DateFormatter = DateFormatter.init()
+    private var calendar: ICKCalendar = ICKCalendar.current
+    
     init() {
+        self.date = Date.init()
+        self.dateFormatter.dateFormat = self.dateFormatterString
+    }
+    
+    init(date: Date) {
+        self.init()
+        self.date = date
+    }
+    
+    init(date: Date, dateFormatterString: String) {
+        self.init(date: date)
+        self.dateFormatterString = dateFormatterString
+    }
+    
+    init(date: Date, dateFormatterString: String, calendar: ICKCalendar) {
+        self.init(date: date, dateFormatterString: dateFormatterString)
+        self.calendar = calendar
+    }
+    
+    public func week() -> Int {
         
+    }
+}
+
+extension ICKDate {
+    
+    public func toDate() -> Date {
+        return self.date
+    }
+    
+    public func toString() -> String {
+        return self.dateFormatter.string(from: self.date)
     }
 }
